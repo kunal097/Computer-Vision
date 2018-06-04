e9caacff6e074a4807e135d002f38edc9a441728
kunal097 <kunalsharma19997@gmail.com>
2018-06-04 19:29:33 +0530

    script to read and write image

--- cv_image.py
+++ cv_image.py
@@ -5,7 +5,7 @@ import cv2
 
 # read image in grayscale mode
 
-img = cv2.imread('d1.jpg',0)
+img = cv2.imread('d1.jpg', 0)
 
 
 # create window
@@ -13,17 +13,17 @@ cv2.namedWindow('Open cv Image')
 
 
 # Show image in the above window
-cv2.imshow('Open cv Image',img)
+cv2.imshow('Open cv Image', img)
 
 
 # Wait unitl key is pressed and record keystoke
 k = cv2.waitKey(0) & 0xFF
 
 
-if k==27:  # Esc is pressed exit without saving
-	# Destroy all windows
-	cv2.destroyAllWindows()
+if k == 27:  # Esc is pressed exit without saving
+    # Destroy all windows
+    cv2.destroyAllWindows()
 
-elif k==115: # S  is pressed therefore save and exit
-	cv2.imwrite('newImg.png',img)
-	cv2.destroyAllWindows()
+elif k == 115:  # S  is pressed therefore save and exit
+    cv2.imwrite('newImg.png', img)
+    cv2.destroyAllWindows()
